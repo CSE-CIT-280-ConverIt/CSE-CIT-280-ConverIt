f15c9d1761780ec9eb9e9376905b05307512d953
Adrian Sandoval <AdrianSandoval@adrians-mbp.attlocal.net>
2017-01-25 19:59:50 -0500

    Initial commit

---
+++ app/src/main/java/com/cse280/adriansandoval/converit/Currency.java
@@ -0,0 +1,14 @@
+package com.cse280.adriansandoval.converit;
+
+/**
+ * Created by AdrianSandoval on 1/25/17.
+ */
+
+public class Currency {
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/cse280/adriansandoval/converit/Length.java
@@ -0,0 +1,8 @@
+package com.cse280.adriansandoval.converit;
+
+/**
+ * Created by AdrianSandoval on 1/25/17.
+ */
+
+public class Length {
+}
