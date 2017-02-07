45adca96c0554f7b310032536279d0fa6aa33b59
Zeyad <zeyadfaeq@gmail.com>
2017-02-07 16:54:19 -0500

    Added string array with currency types to Currency class

--- app/src/main/java/com/cse280/adriansandoval/converit/Currency.java
+++ app/src/main/java/com/cse280/adriansandoval/converit/Currency.java
@@ -5,6 +5,8 @@ package com.cse280.adriansandoval.converit;
  */
 
 public class Currency {
+    // A string array that stores currency types to be loaded to the listview
+  public static  String [] currencyTypes={"Us Dollar","Euro", "GB Pound","Yen","Canadian Dollar"};
 
 
 
