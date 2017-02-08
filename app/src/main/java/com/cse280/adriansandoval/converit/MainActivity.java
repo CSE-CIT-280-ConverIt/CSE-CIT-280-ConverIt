187457d3ed5b1d9482d22451b7baa724ad36cea9
Adsanvar <adriansandovalvargas@gmail.com>
2017-02-07 23:34:22 -0500

    Added functionality to Main Activity. Includes: Setup of ListView and Conversion
    Finished methods for converting Volume Units

--- app/src/main/java/com/cse280/adriansandoval/converit/MainActivity.java
+++ app/src/main/java/com/cse280/adriansandoval/converit/MainActivity.java
@@ -1,13 +1,66 @@
 package com.cse280.adriansandoval.converit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Volume
+    Volume volume = new Volume();
+
+    String[] volumeUnits = new String[]{
+            "Cubic Feet",
+            "Cubic Meter",
+            "Liters",
+            "Gallons",
+            "MilliLiter"
+    };
+
+    //ImageView
+    ImageView VolumeImageView;
+    ImageView showInImageView;
+    ImageView showOutImageView;
+    ImageView convert;
+
+
+
+    //List View
+    ListView inListView;
+    ListView outListView;
+
+
+    // Unit
+    String inUnit;
+    String outUnit;
+
+    //editText
+    EditText input;
+
+    //TextView
+    TextView output;
+
+    //conversion variables
+    public double inValue;
+    public String outValue;
+
+    //option check
+    boolean check = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +69,129 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        check = false;
+
+        //register contents
+        VolumeImageView = (ImageView) findViewById(R.id.volumeImage);
+        showInImageView= (ImageView) findViewById(R.id.showInListView);
+        showOutImageView= (ImageView) findViewById(R.id.showOutListView);
+        convert = (ImageView) findViewById(R.id.convert);
+
+        inListView = (ListView) findViewById(R.id.inListView);
+        outListView = (ListView) findViewById(R.id.outListView);
+
+        input = (EditText) findViewById(R.id.input);
+
+        output = (TextView) findViewById(R.id.outPut);
+
+
+        inListView.setVisibility(View.INVISIBLE);
+        outListView.setVisibility(View.INVISIBLE);
+
 
     }
+
+
+    private void setListView(String[] inString){
+
+        String [] string;
+        string = inString;
+
+        //creates a list from string elements
+        final List<String> Units = new ArrayList<String>(Arrays.asList(string));
+
+        //creates and array adapter from list
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, Units);
+
+        //sets adapter
+        inListView.setAdapter(arrayAdapter);
+        outListView.setAdapter(arrayAdapter);
+
+        //input option handler
+        inListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+                String selectedFromList = (inListView.getItemAtPosition(position).toString());
+
+                inUnit = selectedFromList;
+
+                inListView.setVisibility(View.INVISIBLE);
+                showInImageView.setVisibility(View.VISIBLE);
+                showOutImageView.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        //output option handler
+        outListView.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+
+                String selectedFromList = (outListView.getItemAtPosition(position).toString());
+
+                outUnit = selectedFromList;
+
+                outListView.setVisibility(View.INVISIBLE);
+                showInImageView.setVisibility(View.VISIBLE);
+                showOutImageView.setVisibility(View.VISIBLE);
+            }
+
+
+        });
+
+
+
+    }
+
+
+    public void setVolumeListView(View view){
+
+        check = true;
+
+
+        setListView(volumeUnits);
+
+    }
+
+    public void openInListView(View view){
+
+        inListView.setVisibility(View.VISIBLE);
+        showInImageView.setVisibility(View.INVISIBLE);
+        showOutImageView.setVisibility(View.INVISIBLE);
+    }
+
+    public void openOutListView(View view){
+
+        outListView.setVisibility(View.VISIBLE);
+        showInImageView.setVisibility(View.INVISIBLE);
+        showOutImageView.setVisibility(View.INVISIBLE);
+    }
+
+
+    public void conversion(View view){
+
+
+        if(inUnit == volumeUnits[0].toString() && outUnit== volumeUnits[1].toString()){
+            outValue = input.getText().toString();
+
+            inValue = Double.parseDouble(outValue);
+
+            volume.cfTocm(inValue);
+
+            inValue = volume.cm;
+
+            outValue = String.format("%.5f m\u00B3", inValue);
+
+
+            output.setText(outValue);
+
+        }
+        else{
+
+        }
+
+
+    }
+
 
 }
--- app/src/main/java/com/cse280/adriansandoval/converit/Volume.java
+++ app/src/main/java/com/cse280/adriansandoval/converit/Volume.java
@@ -5,4 +5,167 @@ package com.cse280.adriansandoval.converit;
  */
 
 public class Volume {
+
+
+    double cf;
+    double cm;
+    double li;
+    double gal;
+    double mili;
+
+    public double cfTocm(double cf){
+
+        cm = (cf/35.315);
+
+        return cm;
+    }
+
+    public double cfToli(double cf){
+
+        li = (cf/28.3168);
+
+        return li;
+
+    }
+
+    public double cfTogal(double cf){
+
+        gal = (cf/7.48052);
+
+        return gal;
+
+    }
+
+    public double cfTomili(double cf){
+
+        mili = (cf/28316.8);
+
+        return mili;
+
+    }
+
+
+    public double cmTocf(double cm){
+
+        cf = (cm * 35.315);
+
+        return cf;
+    }
+
+    public double cmToli(double cm){
+
+        li = (cm/1000);
+
+        return li;
+
+    }
+
+    public double cmTogal(double cm){
+
+        gal = (cm/264.172);
+
+        return gal;
+
+    }
+
+    public double cmTomili(double cm){
+
+        mili = (cm/1000000);
+
+        return mili;
+
+    }
+
+    public double liTocf(double li){
+
+        cf = (28.3168 * li);
+
+        return cf;
+    }
+
+    public double liTocm(double li){
+
+        cm = (1000*li);
+
+        return cm;
+
+
+    }
+
+    public double liTogal(double li){
+
+        gal = (li/0.264172);
+        return gal;
+
+    }
+
+    public double liTomili(double li){
+
+        mili = (li/1000);
+
+        return mili;
+
+    }
+
+    public double galTocm(double gal){
+
+        cm = (264.172*gal);
+
+        return cm;
+
+
+    }
+
+    public double galToli(double gal){
+
+        li = (0.264172*gal);
+
+        return li;
+
+    }
+
+    public double galTocf(double gal){
+
+        cf = (7.48052*gal);
+
+        return cf;
+    }
+
+    public double galTomili(double gal){
+
+        mili = (gal/3785.41);
+        return mili;
+
+    }
+
+    public double miliTocm(double mili){
+
+        cm = (1000000*mili);
+        return cm;
+
+    }
+
+    public double miliToli(double mili){
+
+        li = (1000*mili);
+
+        return li;
+
+
+    }
+
+    public double miliTogal(double mili){
+
+        gal = (3785.41*mili);
+
+        return gal;
+
+    }
+
+    public double miliTocf(double mili){
+        cf = (28316.8*mili);
+
+        return cf;
+    }
+
 }
